acccb63cf4589e65fc3c46e3b06d5d99c2225c81
Tushar <tusharutane2@gmail.com>
2023-10-18 23:25:42 +0530

    updated

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 const Register = () => {
-	return <div>Register</div>;
+	return (
+		<div>
+			<div className="">
+				<form action="POST">
+					<input type="text" placeholder="Enter your fullname" />
+					<input type="text" placeholder="Enter you email" />
+					<input type="password" placeholder="Enter your password" />
+					<button>Register your Account</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
