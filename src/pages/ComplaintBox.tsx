5d12b1c92877e3991d2da6c4b51cbe9047ab6536
Tushar <tusharutane2@gmail.com>
2023-10-27 13:26:37 +0530

    Register page done:

--- src/pages/ComplaintBox.tsx
+++ src/pages/ComplaintBox.tsx
@@ -1,7 +1,28 @@
-import React from "react";
-
 const ComplaintBox = () => {
-	return <div>ComplaintBox</div>;
+	return (
+		<div>
+			<h1>Complaint Box</h1>
+			<form>
+				<div>
+					<label htmlFor="">Student's Name</label>
+					<input type="text" placeholder="Write your name" />
+				</div>
+				<div>
+					<label htmlFor="">Gender</label>
+					<div>
+						<button>
+							<span>image male</span>
+							<span>image female</span>
+						</button>
+					</div>
+				</div>
+				<div>
+					<label htmlFor="">Description about Complaint</label>
+					<textarea name="complaint" id="" cols={30} rows={10}></textarea>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default ComplaintBox;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,27 @@
+import { NavLink } from "react-router-dom";
+
 const Register = () => {
 	return (
 		<div>
 			<div className="">
-				<form action="POST">
-					<input type="text" placeholder="Enter your fullname" />
+				<form>
+					<label htmlFor=""> First name</label>
+					<input type="text" placeholder="Enter your first name" />
+					<label htmlFor="">Last name</label>
+					<input type="text" placeholder="Enter your last name" />
+					<label htmlFor="">Email</label>
 					<input type="text" placeholder="Enter you email" />
+					<label htmlFor="">Password</label>
 					<input type="password" placeholder="Enter your password" />
+					<label htmlFor="">Confirm password</label>
+					<input type="password" placeholder="Enter your confirm password" />
 					<button>Register your Account</button>
+					<div>
+						<span>Already have an account?</span>
+						<NavLink to={"/login"}>
+							<button>Login</button>
+						</NavLink>
+					</div>
 				</form>
 			</div>
 		</div>
